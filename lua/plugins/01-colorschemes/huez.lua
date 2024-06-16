802e56bfb4d32a740c7dbb4a01efd5f6c8f53b81
Ivan Curtiñez <ivan.curtinez@gmail.com>
2024-06-16 15:56:55 -0400

    INSTALL: add huez for colorschemes

---
+++ lua/plugins/01-colorschemes/huez.lua
@@ -0,0 +1,11 @@
+-- Lazy
+return {
+	"vague2k/huez.nvim",
+	-- if you want registry related features, uncomment this
+	import = "huez-manager.import",
+	config = function()
+		require("huez").setup({
+			fallback = "default",
+		})
+	end,
+}
