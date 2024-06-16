6ebf4f464051eb8c67f2a765328cbd072ec7ecf4
Ivan Curtiñez <ivan.curtinez@gmail.com>
2024-06-16 12:37:53 -0400

    INSTALL: add buffer line and lualine

---
+++ lua/plugins/04-auto-complete/cmp-nvim.lua
@@ -0,0 +1,104 @@
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"hrsh7th/cmp-path", -- source for file system paths
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+			build = "make install_jsregexp",
+		},
+		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		"rafamadriz/friendly-snippets", -- useful snippets
+		"onsails/lspkind.nvim", -- vs-code like pictograms
+	},
+	config = function()
+		local cmp = require("cmp")
+		local lspkind = require("lspkind")
+		local luasnip = require("luasnip")
+
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+		require("luasnip.loaders.from_vscode").lazy_load()
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+			},
+			snippet = { -- configure how nvim-cmp interacts with snippet engine
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = function(fallback)
+					if not cmp.select_next_item() then
+						if vim.bo.buftype ~= "prompt" and has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end
+				end,
+
+				["<S-Tab>"] = function(fallback)
+					if not cmp.select_prev_item() then
+						if vim.bo.buftype ~= "prompt" and has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end
+				end,
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+			}),
+			-- sources for autocompletion
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" }, -- text within current buffer
+				{ name = "path" }, -- file system paths
+			}),
+			---@diagnostic disable: missing-fields
+			-- configure lspkind for vs-code like pictograms in completion menu
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+					local strings = vim.split(kind.kind, "%s", { trimempty = true })
+					kind.kind = " " .. (strings[1] or "") .. " "
+					kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+					return kind
+				end,
+			},
+			window = {
+				completion = {
+					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+					col_offset = -3,
+					side_padding = 0,
+					border = "double",
+				},
+				documentation = {
+					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+					border = "rounded",
+				},
+			},
+		})
+		-- Customization for Pmenu
+		vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
+		vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "NONE" })
+
+		vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+		vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+		vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+		vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+	end,
+}
