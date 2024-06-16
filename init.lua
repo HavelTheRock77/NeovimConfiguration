38ac47c7df021dc8fd040d31d59d90ebff8aa868
Ivan Curtiñez <ivan.curtinez@gmail.com>
2024-06-16 12:38:55 -0400

    FIX: add plugoins route to init.lua

--- init.lua
+++ init.lua
@@ -4,21 +4,11 @@ require('keymaps')
 require('autocommands')
 require('lazy-config')
 
-require("lazy").setup({
-	{ 
-		"catppuccin/nvim", name = "catppuccin", priority = 1000 
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup {}
-		end,
+require('lazy').setup({
+	spec = {
+		{ import = 'plugins.01-colorschemes' },
+		{ import = 'plugins.02-file-explorer' },
+		{ import = 'plugins.03-ui-nvim' },
+		{ import = 'plugins.04-auto-complete' },
 	}
 })
-
-vim.cmd.colorscheme('catppuccin')
